beb88d9c3bf21f081a57d006320400bb1234827c
Amir Omidi <amir@aaomidi.com>
2018-03-08 01:49:20 -0500

    Part of #4

--- src/main/java/io/github/cs451/ge/adapter/SelectionAction.java
+++ src/main/java/io/github/cs451/ge/adapter/SelectionAction.java
@@ -1,12 +1,10 @@
 package io.github.cs451.ge.adapter;
 
 import io.github.cs451.ge.game.Coordinate;
+import lombok.Data;
 
 // This is the action when the user has only selected a piece
+@Data
 public class SelectionAction implements Action {
     private final Coordinate location;
-
-    public SelectionAction(Coordinate location) {
-        this.location = location;
-    }
 }
