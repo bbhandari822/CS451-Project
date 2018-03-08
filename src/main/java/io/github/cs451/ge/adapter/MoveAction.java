c4339d6fd56454a955171b1a4935488a1b2a32ef
Amir Omidi <amir@aaomidi.com>
2018-03-08 01:47:04 -0500

    closes #4

--- src/main/java/io/github/cs451/ge/adapter/MoveAction.java
+++ src/main/java/io/github/cs451/ge/adapter/MoveAction.java
@@ -1,6 +1,11 @@
 package io.github.cs451.ge.adapter;
 
-// This is the action when the user finally decides to move a piece
-public class MoveAction implements Action {
+import io.github.cs451.ge.game.Coordinate;
+import lombok.Data;
 
+// This is the action when the user finally decides to move a piece
+@Data
+public class MoveAction implements Action {
+    private final Coordinate from;
+    private final Coordinate to;
 }
--- src/main/java/io/github/cs451/ge/adapter/SelectionAction.java
+++ src/main/java/io/github/cs451/ge/adapter/SelectionAction.java
@@ -1,5 +1,12 @@
 package io.github.cs451.ge.adapter;
 
+import io.github.cs451.ge.game.Coordinate;
+
 // This is the action when the user has only selected a piece
 public class SelectionAction implements Action {
+    private final Coordinate location;
+
+    public SelectionAction(Coordinate location) {
+        this.location = location;
+    }
 }
